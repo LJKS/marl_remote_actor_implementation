e639edc82b5e0115d104efd05e72e99e29524a57
LJKS <lschmid@uos.de>
2020-09-18 15:47:05 +0200

    save test results implemented

--- test_script.py
+++ test_script.py
@@ -6,4 +6,7 @@ own = pickle.load(open(own_path, 'rb'))
 print(own.gym)
 #list of paths to test against
 test_paths = [own_path]
-results = own.test_against(test_paths, num_tests=5, num_cpus=4)
+results = own.test_against(test_paths, num_tests=5, num_cpus=8, mode=[0,10,20])
+result_path = 'saves/saved_results/'
+save_name = '2020-08-03_00:19:38.545881_self_evaluation'
+pickle.dump( results, open( result_path+save_name, "wb" ) )
